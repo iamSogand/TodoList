401fa7299e1021a6dd6e254ccb86810027804e3d
imSogand <imsogand@outlook.com>
2023-08-03 20:33:18 +0330

    define constants

---
+++ assets/scripts/app.js
@@ -0,0 +1,6 @@
+const addItemBtn = document.getElementById('addBtn');
+const userInput = document.getElementById('input');
+const entryText = document.getElementById('entry-text');
+
+const items = [];
+
