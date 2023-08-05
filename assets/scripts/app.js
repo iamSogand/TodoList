68bd109befcbf6665a817b66d01fa15ec7dd1802
imSogand <imsogand@outlook.com>
2023-08-05 21:40:04 +0330

    define addItemHandler function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,3 +4,49 @@ const entryText = document.getElementById('entry-text');
 
 const items = [];
 
+function renderNewItemElement(id, title) {
+  const newItemElement = document.createElement('li');
+  newItemElement.innerHTML = `
+    <div>
+    <li class="list-group-item">${title}</li>
+    </div>`;
+  newItemElement.addEventListener('click', deleteItemHandler);
+  const listRoot = document.getElementById('item-list');
+  listRoot.append(newItemElement);
+}
+
+function clearInput() {
+  userInput.value = '';
+}
+
+function deleteItemHandler(itemId) {
+  let itemIndex = 0;
+  for (const item of items) {
+    if (item.id === itemId) {
+      break;
+    }
+    itemIndex++;
+  }
+  items.splice(itemIndex, 1);
+  const listRoot = document.getElementById('item-list');
+  listRoot.children[itemIndex].remove();
+}
+
+function addItemHandler() {
+  const titleValue = userInput.value;
+
+  if (titleValue.trim() === '') {
+    alert('Please enter valid value');
+    return;
+  }
+  const newItem = {
+    id: Math.random().toString(),
+    title: titleValue,
+  };
+
+  items.push(newItem);
+  renderNewItemElement(newItem.id, newItem.title);
+  clearInput();
+}
+
+addItemBtn.addEventListener('click', addItemHandler);
